dfebeef23f0ee9237abb29ec5c75a4ce629416df
Aman Suffa <amansuffah@gmail.com>
2025-10-19 13:18:03 +0500

    created tempelate parser

--- src/models/EmailTemplate.js
+++ src/models/EmailTemplate.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
-const EmailTemplateSchema = new mongoose.Schema({
-  name: String,
-  subject: String,
-  body: String,
-  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
-  isDefault: { type: Boolean, default: false } // <-- new field
+// const EmailTemplateSchema = new mongoose.Schema({
+//   name: String,
+//   subject: String,
+//   body: String,
+//   ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+//   isDefault: { type: Boolean, default: false } // <-- new field
+// }, { timestamps: true });
+
+
+const emailTemplateSchema = new mongoose.Schema({
+  name: { type: String, required: true }, 
+  type: { type: String, required: true }, 
+  subject: { type: String, required: true },
+  body: { type: String, required: true }, 
 }, { timestamps: true });
 
 
