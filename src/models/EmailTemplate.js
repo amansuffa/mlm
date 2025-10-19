4ccc8d4af29fefb2b6fafe321ba7931a8a836f6d
Aman Suffa <amansuffah@gmail.com>
2025-10-19 14:15:41 +0500

    added email templates in db

--- src/models/EmailTemplate.js
+++ src/models/EmailTemplate.js
@@ -8,13 +8,17 @@ import mongoose from "mongoose";
 //   isDefault: { type: Boolean, default: false } // <-- new field
 // }, { timestamps: true });
 
+const emailTemplateSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    subject: { type: String, required: true },
+    body: { type: String, required: true },
+    isDefault: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-const emailTemplateSchema = new mongoose.Schema({
-  name: { type: String, required: true }, 
-  type: { type: String, required: true }, 
-  subject: { type: String, required: true },
-  body: { type: String, required: true }, 
-}, { timestamps: true });
-
-
-export const EmailTemplate = mongoose.models.EmailTemplate || mongoose.model("EmailTemplate", EmailTemplateSchema);
+export const EmailTemplate =
+  mongoose.models.EmailTemplate ||
+  mongoose.model("EmailTemplate", emailTemplateSchema);
