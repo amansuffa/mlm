b8edefcfce5fd832d778d16571b18483c3c915ce
Aman Suffa <amansuffah@gmail.com>
2025-11-03 17:37:31 +0500

    fix: layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default async function RootLayout({ children }) {
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem={true}
           disableTransitionOnChange={false}
         >
